f9e55325e5bc8effa1043aba447e3d9499ea4023
Gabriel Siqueira Gama (zhant) <102556033+GabrielSiqueiraGama@users.noreply.github.com>
2024-06-14 14:35:24 -0300

    Update rotas.py

--- rotas.py
+++ rotas.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from math import radians, sin, cos, sqrt, atan2
 import plotly.graph_objs as go
+import heapq
+from itertools import permutations
 
 # Função para calcular a distância entre dois pontos usando a fórmula de Haversine
 def calcular_distancia(coord1, coord2):
@@ -14,8 +16,89 @@ def calcular_distancia(coord1, coord2):
     distance = R * c
     return distance
 
-# Carregando os dados dos estados
-states = pd.read_csv("https://raw.githubusercontent.com/GabrielSiqueiraGama/bairrosBalsas/main/enderecos.csv?token=GHSAT0AAAAAACPBPLDDEAI2MKNS7IIA4UXOZQZYYPA")
+# Função A*
+def astar(start_index, goal_index, states):
+    class Node:
+        def __init__(self, index, parent=None, g=0, h=0):
+            self.index = index  # Índice do nó
+            self.parent = parent  # Nó pai
+            self.g = g  # Custo atual do nó até o ponto de partida
+            self.h = h  # Heurística (distância estimada do nó até o ponto de destino)
+        
+        def f(self):
+            return self.g + self.h
+        
+        def __lt__(self, other):
+            return self.f() < other.f()
+    
+    start_node = Node(start_index)
+    goal_node = Node(goal_index)
+    
+    open_set = []  # Conjunto de nós a serem explorados
+    heapq.heappush(open_set, start_node)
+    closed_set = set()  # Conjunto de nós já explorados
+    
+    while open_set:
+        current_node = heapq.heappop(open_set)
+        
+        if current_node.index == goal_node.index:
+            # Reconstruir o caminho a partir do nó objetivo
+            path = []
+            while current_node:
+                path.append(current_node.index)
+                current_node = current_node.parent
+            return path[::-1]  # Inverter o caminho
+            
+        closed_set.add(current_node.index)
+        
+        for neighbor_index in states.loc[current_node.index, 'Neighbors'].split(','):
+            neighbor_index = int(neighbor_index)
+            if neighbor_index in closed_set:
+                continue
+            
+            neighbor_node = Node(neighbor_index)
+            neighbor_node.parent = current_node
+            neighbor_node.g = current_node.g + calcular_distancia(
+                (states.loc[current_node.index, 'Latitude'], states.loc[current_node.index, 'Longitude']),
+                (states.loc[neighbor_index, 'Latitude'], states.loc[neighbor_index, 'Longitude'])
+            )
+            neighbor_node.h = calcular_distancia(
+                (states.loc[neighbor_index, 'Latitude'], states.loc[neighbor_index, 'Longitude']),
+                (states.loc[goal_index, 'Latitude'], states.loc[goal_index, 'Longitude'])
+            )
+            
+            if not any(node.index == neighbor_node.index for node in open_set):
+                heapq.heappush(open_set, neighbor_node)
+    
+    return None  # Caminho não encontrado
+
+# Função para calcular o custo total de um caminho
+def calcular_custo_total(path, states):
+    total_cost = 0
+    for i in range(len(path) - 1):
+        total_cost += calcular_distancia(
+            (states.loc[path[i], 'Latitude'], states.loc[path[i], 'Longitude']),
+            (states.loc[path[i + 1], 'Latitude'], states.loc[path[i + 1], 'Longitude'])
+        )
+    return total_cost
+
+# Função para encontrar o melhor caminho passando por pontos intermediários
+def encontrar_melhor_caminho(partida_index, destino_index, intermediarios, states):
+    melhor_caminho = None
+    menor_custo = float('inf')
+    
+    for perm in permutations(intermediarios):
+        caminho_atual = [partida_index] + list(perm) + [destino_index]
+        custo_atual = calcular_custo_total(caminho_atual, states)
+        
+        if custo_atual < menor_custo:
+            menor_custo = custo_atual
+            melhor_caminho = caminho_atual
+    
+    return melhor_caminho
+
+# Carregar os dados dos estados
+states = pd.read_csv("https://raw.githubusercontent.com/GabrielSiqueiraGama/bairrosBalsas/main/estados_com_vizinhos.csv")
 
 # Solicitar ao usuário o índice do ponto de entrada
 partida_index = int(input("Digite o índice do ponto de entrada: "))
@@ -25,32 +108,20 @@ print(f"Índice do ponto de entrada: {partida_index}")
 destino_index = int(input("Digite o índice do ponto de saída: "))
 print(f"Índice do ponto de saída: {destino_index}")
 
-# Lista de nós intermediários que a rota deve passar
-nos_intermediarios = [9]  # Exemplo de nós intermediários
-print("Nós intermediários: ", nos_intermediarios)
+# Solicitar ao usuário os índices dos pontos intermediários (separados por vírgula)
+intermediarios_input = input("Digite os índices dos pontos intermediários separados por vírgula (ou deixe em branco se não houver): ")
+intermediarios = [int(x) for x in intermediarios_input.split(',')] if intermediarios_input else []
+print(f"Índices dos pontos intermediários: {intermediarios}")
 
-# Inicializando a rota ótima
-optimal_path = [partida_index]
+# Encontrar o melhor caminho usando permutações dos pontos intermediários
+melhor_caminho = encontrar_melhor_caminho(partida_index, destino_index, intermediarios, states)
 
-# Calculando a distância total percorrida
-total_distance = 0
+if melhor_caminho:
+    print("Melhor rota encontrada:", melhor_caminho)
+else:
+    print("Não foi possível encontrar uma rota.")
 
-# Adicionando os nós intermediários à rota ótima
-for node in nos_intermediarios:
-    distance = calcular_distancia((states.loc[optimal_path[-1], 'Latitude'], states.loc[optimal_path[-1], 'Longitude']),
-                                  (states.loc[node, 'Latitude'], states.loc[node, 'Longitude']))
-    total_distance += distance
-    optimal_path.append(node)
-
-# Adicionando o ponto de saída à rota ótima
-distance = calcular_distancia((states.loc[optimal_path[-1], 'Latitude'], states.loc[optimal_path[-1], 'Longitude']),
-                              (states.loc[destino_index, 'Latitude'], states.loc[destino_index, 'Longitude']))
-total_distance += distance
-optimal_path.append(destino_index)
-
-print("Distância total percorrida:", total_distance, "km")
-
-# Plotagem do mapa com a rota ótima
+# Plotagem do mapa com a melhor rota
 mapbox_token = "pk.eyJ1IjoiemhhbnR0IiwiYSI6ImNsdjFqbXFiMzA1aXcybmxkcHd1Ym5zajYifQ.Hcys5Sf519pfI_BauT5iVA"
 
 trace1 = go.Scattermapbox(
@@ -66,18 +137,18 @@ trace1 = go.Scattermapbox(
 )
 
 trace2 = go.Scattermapbox(
-    lat=[states.loc[i, 'Latitude'] for i in optimal_path],
-    lon=[states.loc[i, 'Longitude'] for i in optimal_path],
+    lat=[states.loc[i, 'Latitude'] for i in melhor_caminho],
+    lon=[states.loc[i, 'Longitude'] for i in melhor_caminho],
     mode='lines',
     line=dict(
-        color='red',  # Vermelho para a rota ótima
+        color='red',  # Vermelho para a melhor rota
         width=2
     ),
-    text=[f"{states.loc[i, 'City']}, {states.loc[i, 'State']}" for i in optimal_path]
+    text=[f"{states.loc[i, 'City']}, {states.loc[i, 'State']}" for i in melhor_caminho]
 )
 
 layout = go.Layout(
-    title='Rota Ótima do Ponto de Entrada para o Destino',
+    title='Melhor Rota do Ponto de Entrada para o Destino',
     width=800,  # Defina a largura desejada em pixels
     height=800,  # Defina a altura desejada em pixels
     hovermode='closest',
